2f7edd19e5147f88506bfbd270fdcedafbff979e
Matia <matia.casu@gmail.com>
2021-03-18 16:21:39 +0100

    Input aggiunti in lista tramite tasto Invio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-  // stampa a schermo la lista
+  // Stampa a schermo la lista
   var list = ["comprare la pasta", "pulire la stanza", "dare l'acqua alle piante", "fare la lavatrice"];
   var bodyList = $("ul.list");
 
@@ -8,7 +8,24 @@ $(function(){
     bodyLi.prepend(list[i]);
     bodyList.append(bodyLi);
   }
-  // stampa a schermo la lista
+  // Stampa a schermo la lista
+
+  // Aggiungi valore input alla lista tramite tasto invio
+  $(".wrapper input").keydown(
+    function(evento){
+      if(evento.which == 13){
+        var input = $(this).val();
+        var bodyLi = $(".template > li").clone();
+
+        bodyLi.prepend(input);
+        $("ul.list").append(bodyLi);
+        $(this).val("");
+      }
+    }
+
+  );
+
+  // /Aggiungi valore input alla lista tramite tasto invio
 
 
 });
